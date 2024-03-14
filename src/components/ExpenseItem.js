a264000a606032f13023251b5d72c6cf1203ffb4
reqadh <128897272+reqadh@users.noreply.github.com>
2024-03-14 23:43:47 +0300

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
-
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiPlus, TiMinus } from 'react-icons/ti'; // Importing TiPlus and TiMinus
 import { AppContext } from '../context/AppContext';
+import currencySymbols from '../currencySymbols'; 
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext); 
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -13,25 +13,51 @@ const ExpenseItem = (props) => {
         });
     };
 
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
+    const increaseAllocation = () => {
         dispatch({
             type: 'ADD_EXPENSE',
-            payload: expense
+            payload: {
+                name: props.name,
+                cost: 10,
+            },
         });
+    }
 
+    const decreaseAllocation = () => {
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: {
+                name: props.name,
+                cost: 10,
+            },
+        });
     }
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{currencySymbols[currency]}{props.cost}</td>
+            <td className="text-center">
+                <button 
+                    onClick={increaseAllocation} 
+                    className="btn btn-success rounded-circle"
+                    style={{ width: '30px', height: '30px', padding: '0', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                >
+                    <TiPlus size='1.5em' />
+                </button>
+            </td>
+            <td className="text-center">
+                <button 
+                    onClick={decreaseAllocation} 
+                    className="btn btn-danger rounded-circle"
+                    style={{ width: '30px', height: '30px', padding: '0', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                >
+                    <TiMinus size='1.5em' />
+                </button>
+            </td>
+            <td className="text-center">
+                <TiDelete size='1.5em' onClick={handleDeleteExpense} style={{cursor: 'pointer', color: 'red'}} />
+            </td>
         </tr>
     );
 };
